d3734c26f4b65a4c65d28207d6ff2a30b7597bf0
sumirart <akun.pratamasumirat@gmail.com>
2019-04-05 13:36:20 +0700

    initial commit - complete

---
+++ src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>This is the TodoList app that I created from following the Vue crash course in Youtube from Traversy Media</p>
+  </div>
+</template>
+
+<style scoped>
+.about {
+  text-align: center;
+  margin-top: 10px;
+}
+p {
+  margin-top: 20px;
+}
+</style>
